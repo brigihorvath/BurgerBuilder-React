4cb097f873aef41f78a673d7e8cf8b077498de2f
Brigitta Horváth <brigi.horvath82@gmail.com>
2019-10-07 09:40:01 +0200

    Navigation - desktop, vertical

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import styles from '../Layout/Layout.module.css';
 
 const Layout = ( props ) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={styles.content}>
             {props.children}
         </main>
